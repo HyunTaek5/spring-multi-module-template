5778936b2681d25bc252bb42edce9ce8ef1c846e
HyunTaek Oh <oht222@naver.com>
2022-10-25 00:37:13 +0900

    refact: ROLE_ADMIN -> ADMIN으로 변경

--- domain/src/main/java/com/template/persistence/user/entity/Role.java
+++ domain/src/main/java/com/template/persistence/user/entity/Role.java
@@ -3,7 +3,7 @@ package com.template.persistence.user.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-  ROLE_ADMIN,
+  ADMIN,
   USER;
 
   public String getAuthority() { return name(); }
